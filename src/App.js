250d883da43fb2d5674042272e6b34f84aef1efd
CARLOS EDUARDO RUIZ SANTOS <carlos.ersantos4@senacsp.edu.br>
2022-03-17 21:51:55 -0300

    Aula-1703 React + Graphql

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ALL_TODOS = gql`
+query allTodos {
+    todos {
+      id
+      completa
+      texto
+    }
+  }`
+    ;
 
-export default App;
+const ATUALIZA_TODO = gql`
+  mutation completeTodo($id: uuid!, $completa: Boolean!) {
+    update_todos(where:
+         {id: {_eq: $id}},
+         _set: {completa: $completa}) {
+      returning {
+        completa
+        texto
+        id
+      }
+    }
+  }
+  `;
+const INSERE_TODO = gql`
+  mutation insertTodos($texto:String) {
+      insert_todos(objects: {texto: $texto}) {
+      returning {
+          id
+          texto
+          completa
+      }
+      }
+  }  `
+    ;
+    const DELETE_TODO = gql`
+    mutation deleteTodos($id:uuid!) {
+        delete_todos(where: {id: {_eq: $id}}) {
+        returning {
+            id
+            texto
+            completa
+        }
+        }
+    } `
+;
+
+export default function App() {
+
+    const [todoTexto, setTodoTexto] = React.useState('');
+    const { data, loading, error } = useQuery(ALL_TODOS);
+    const [update_todos] = useMutation(ATUALIZA_TODO);
+    const [insereTodo] = useMutation(INSERE_TODO);
+    const [deleteTodo] = useMutation(DELETE_TODO);
+
+    async function handleconcluirTarefa(id, completa) {
+
+        await update_todos({ variables: { id, completa: !completa } })
+    }
+
+    async function handleInsereTodo(event) {
+        event.preventDefault();
+        await insereTodo({
+            variables: { texto: todoTexto },
+            refetchQueries: [{ query: ALL_TODOS }]
+        });
+        setTodoTexto('');
+    }
+    async function handleDeleteTodo(id) {
+        if (window.confirm("deseja apagar esse Todo?")) {
+            await deleteTodo({
+                variables: { id },
+                refetchQueries: [{ query: ALL_TODOS }]
+            })
+        }
+    }
+
+    if (loading) return <h1>Carregando</h1>;
+    if (error) return <h1>Não foi possivel pegar os dados...</h1>
+
+    console.log(data)
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+            <form onSubmit={handleInsereTodo}>
+                <input type="text" placeholder="Digite a nova tarefa" value={todoTexto} onChange={(event) => setTodoTexto(event.target.value)} />
+                <button type="submit">Salvar a Tarefa</button>
+            </form>
+            <hr />
+            {data.todos.map((todo) =>
+                <div onDoubleClick={() => handleconcluirTarefa(todo.id, todo.completa)} key={todo.id}>
+                    <span className={todo.completa ? 'striket' : ''}>{todo.texto}</span>
+                    <button onClick={() => handleDeleteTodo(todo.id)}>Remover</button>
+                </div>)}
+        </div>)
+
+
+}      
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new ApolloClient({
+    uri: 'https://correct-maggot-75.hasura.app/v1/graphql',
+    headers: {
+        'x-hasura-admin-secret': 'YKKOTAyD4vHCe81H4ZRvBT6bXLNCg2mxIv9q0WkIokzD7247zmrQpUzjol0G2lJ2'
+    },
+    cache: new InMemoryCache()
+});
+
+console.log(client)
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App/>
+</ApolloProvider>, document.getElementById('root'))
